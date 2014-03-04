b34f22515111058dfc351a7c9fedbba2f67b135b
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2014-03-04 12:42:35 -0400

    update documentation added how to for CE rocks commands

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,5 +1,5 @@
 ROLL			= osg
-VERSION			= 3.1.22
+VERSION			= 3.1.29
 NAME    		= roll-$(ROLL)-usersguide
 RELEASE			= 0
 MANUALRELEASE 		= V7_8_8
@@ -8,6 +8,6 @@ SUMMARY_COMPATIBLE	= $(VERSION)
 SUMMARY_MAINTAINER	= Rocks Group
 SUMMARY_ARCHITECTURE	= i386, x86_64
 
-ROLL_REQUIRES		= base kernel os
-ROLL_CONFLICTS		=
+ROLL_REQUIRES		= base kernel os ganglia
+ROLL_CONFLICTS		= condor
 
