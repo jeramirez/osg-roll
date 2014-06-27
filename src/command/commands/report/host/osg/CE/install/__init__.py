ade47f75223aff02b19d447efda8f45624b82a01
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2014-06-27 15:27:29 -0400

    added two new commands:  rocks report host osg CE install <host>  rocks sync host osg CE install <host> updated documentation to include new commands.

---
+++ src/command/commands/report/host/osg/CE/install/__init__.py
@@ -0,0 +1,91 @@
+#$Id$
+# 
+# @Copyright@
+# 
+# $Log$
+# Revision 0.10  2012/10/26 05:48:54  eduardo
+# Creation
+#
+
+import sys
+import os
+import pwd
+import string
+import types
+import rocks.commands
+from syslog import syslog
+
+class Command(rocks.commands.HostArgumentProcessor,
+	rocks.commands.report.command):
+	"""
+
+	Output the OSG CE wrapper install script
+
+	<arg type='string' name='host'>
+	One host name.
+	</arg>
+
+	<example cmd='report host osg CE install ce-0-0'>
+	Create wrapper script to install OSG CE for ce-0-0
+	</example>
+
+	"""
+
+	def run(self, params, args):
+
+		self.beginOutput()
+
+                for host in self.getHostnames(args):
+			self.host = host
+			osg_ce     = self.db.getHostAttr(host,'OSG_CE')
+			osg_gums   = self.db.getHostAttr(host,'OSG_GumsServer')
+			osg_export = self.db.getHostAttr(host,'OSG_CE_Export_LocalDir')
+			ksphost    = self.db.getHostAttr(host,'Kickstart_PrivateKickstartHost')
+			kspn       = self.db.getHostAttr(host,'Kickstart_PrivateNetwork')
+			kspm       = self.db.getHostAttr(host,'Kickstart_PrivateNetmask')
+			if osg_ce > 0:
+				self.addOutput(self.host, '/usr/sbin/groupadd -g &OSG_tomcatgid; tomcat')
+				self.addOutput(self.host, '/usr/sbin/useradd -u &OSG_tomcatuid; -g &OSG_tomcatgid; -c "Tomcat" -s /bin/sh -d -m /usr/share/tomcat&rocks_version_major; tomcat')
+				self.addOutput(self.host, '/usr/sbin/groupadd -g &OSG_gratiagid; gratia')
+				self.addOutput(self.host, '/usr/sbin/useradd -u &OSG_gratiauid; -g &OSG_gratiagid; -c "gratia runtime user" -s /sbin/nologin -d /etc/gratia gratia')
+				self.addOutput(self.host, 'touch /var/log/ce-install.log')
+				self.addOutput(self.host, 'yum install osg-ca-certs  &gt;&gt; /var/log/ce-install.log 2&gt;&amp;1')
+				self.addOutput(self.host, 'yum install osg-ce-%s &gt;&gt; /var/log/ce-install.log 2&gt;&amp;1' % (osg_ce))
+				self.addOutput(self.host, 'yum install globus-gram-job-manager-managedfork  &gt;&gt; /var/log/ce-install.log 2&gt;&amp;1')
+				self.addOutput(self.host, '')
+				self.addOutput(self.host, '#Make sure config templates exists')
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/01-squid.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/01-squid.ini /etc/osg/config.d/01-squid.ini.template')
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/10-misc.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/10-misc.ini /etc/osg/config.d/10-misc.ini.template')
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/10-storage.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/10-storage.ini /etc/osg/config.d/10-storage.ini.template')
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/15-managedfork.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/15-managedfork.ini /etc/osg/config.d/15-managedfork.ini.template')
+				self.addOutput(self.host, '[ -f /etc/osg/config.d/20-%s.ini ]&amp;&amp;[ ! -f /etc/osg/config.d/20-%s.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/20-%s.ini /etc/osg/config.d/20-%s.ini.template' % (osg_ce,osg_ce,osg_ce,osg_ce))
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/30-gip.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/30-gip.ini /etc/osg/config.d/30-gip.ini.template')
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/40-network.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/40-network.ini /etc/osg/config.d/40-network.ini.template')
+				self.addOutput(self.host, '[ ! -f /etc/osg/config.d/40-siteinfo.ini.template ]&amp;&amp;cp -p /etc/osg/config.d/40-siteinfo.ini /etc/osg/config.d/40-siteinfo.ini.template')
+				self.addOutput(self.host, '')
+				if osg_gums > 0:
+					self.addOutput(self.host, '#Set Gums Client')
+					self.addOutput(self.host, 'sed -i -e "s#yourgums.yourdomain#%s#" /etc/lcmaps.db' % osg_gums )
+					self.addOutput(self.host, 'sed -i -e "s@localhost@%s@" /etc/gums/gums-client.properties' % osg_gums )
+					self.addOutput(self.host, 'sed -i -e "s@# globus_mapping@globus_mapping@" /etc/grid-security/gsi-authz.conf' )
+					self.addOutput(self.host, '')
+				if osg_export > 0:
+					self.addOutput(self.host, '# Rocks attr OSG_CE_Export_LocalDir = %s' % osg_export )
+					self.addOutput(self.host, '#     means CE LocalDir = %s' % osg_export )
+					self.addOutput(self.host, '# create if needed %s/ce/globus/share' % osg_export )
+					self.addOutput(self.host, '[ -d %s/ce/globus/share ]||mkdir -p %s/ce/globus/share' % (osg_export,osg_export) )
+					self.addOutput(self.host, '[ -d %s/ce/globus/share/certificates ]||mv /etc/grid-security/certificates %s/ce/globus/share/.' % (osg_export,osg_export) )
+					self.addOutput(self.host, '[ -d /etc/grid-security/certificates ]||ln -s %s/ce/globus/share/certificates /etc/grid-security/certificates' % osg_export )
+					self.addOutput(self.host, '# create if needed %s/app/etc' % osg_export )
+					self.addOutput(self.host, '[ -d %s/app/etc ]||mkdir -p %s/app/etc' % (osg_export,osg_export) )
+					self.addOutput(self.host, 'chmod 1777 %s/app' % osg_export )
+					self.addOutput(self.host, 'chmod 1777 %s/app/etc' % osg_export )
+					self.addOutput(self.host, '# if needed add %s/ce to /etc/export' % osg_export )
+					self.addOutput(self.host, '[ "`grep -c %s/ce /etc/exports`" != "0" ]||echo "%s/ce %s(rw,async,no_root_squash) %s/%s(rw,async)" &gt;&gt; /etc/export ' % (osg_export,osg_export,ksphost,kspn,kspm) )
+					self.addOutput(self.host, '# if needed add %s/app to /etc/export' % osg_export )
+					self.addOutput(self.host, '[ "`grep -c %s/app /etc/exports`" != "0" ]||echo "%s/app %s(rw,async,no_root_squash) %s/%s(rw,async)" &gt;&gt; /etc/export ' % (osg_export,osg_export,ksphost,kspn,kspm) )
+				self.addOutput(self.host, '')
+				self.addOutput(self.host, 'touch /var/tmp/globus-port-state.log')
+				self.addOutput(self.host, '')
+
+		self.endOutput(padChar='')
---
+++ src/command/commands/sync/host/osg/CE/install/__init__.py
@@ -0,0 +1,119 @@
+# $Id$
+# 
+# @Copyright@
+#
+# $Log$
+# Revision 0.0  2012/11/09 05:48:55  eduardo
+# Copyright Storm for Mamba
+#
+
+import os
+import time
+import rocks.commands
+import threading
+
+max_threading = 512
+timeout = 30.0
+
+class Parallel(threading.Thread):
+	def __init__(self, cmd):
+		threading.Thread.__init__(self)
+		self.cmd = cmd
+
+	def run(self):
+		os.system(self.cmd)
+
+
+class Command(rocks.commands.sync.host.command):
+	"""
+	Install OSG CE on the named hosts.
+
+	<param type="bool" name="test">
+	If want to test output set this parameter.
+	Default is no.
+	</param>
+
+
+	<example cmd='sync host osg CE install ce-0-0'>
+	add tomcat user, add tomcat group, add gratia group and install osg-ce-certs, osg-ce-"OSG_CE", globus-gram-job-manager-managedfork 
+        on host ce-0-0 if attr OSG_CE is set in this host.
+	</example>
+
+	<example cmd='sync host osg CE install ce-0-0 test=yes'>
+	Show the bash script that will run to install osg-ce-"OSG_CE" on host ce-0-0
+	</example>
+	"""
+
+	def run(self, params, args):
+		hosts = self.getHostnames(args, managed_only=1)
+		istest, = self.fillParams([ ('test', 'n') ])
+		istest = self.str2bool(istest)
+
+		threads = []
+		for host in hosts:
+			if max_threading > 0:
+				while threading.activeCount() > max_threading:
+					#
+					# need to wait for some threads to
+					# complete before starting any new ones
+					#
+					time.sleep(0.001)
+
+			#
+			# get the attributes for the host
+			#
+			attrs     = self.db.getHostAttrs(host)
+			is_osg_ce = self.db.getHostAttr(host,'OSG_CE')
+			portrange = self.db.getHostAttr(host,'OSG_GlobusPortRange')
+			cmd = '/opt/rocks/bin/rocks report host osg CE install '
+			cmd += '%s | ' % host
+			cmd += '/opt/rocks/bin/rocks report script '
+
+			if istest:
+				cmd += 'attrs="%s" ' % attrs
+				cmd += ';echo " | ssh %s bash" ' % host
+				if is_osg_ce>0 :
+					cmd += ';echo " [OSG_CE =%s] will run on host %s" ' % (is_osg_ce,host)
+				else:
+					cmd += ';echo " OSG_CE not set on %s will not run" ' % host
+			else:
+				cmd += 'attrs="%s" | ' % attrs
+				cmd += 'ssh %s bash > /dev/null 2>&1 ' % host
+
+			if is_osg_ce>0:
+				if istest:
+					cmd += ';echo'
+				else:
+					cmd += ';'
+				cmd +=' /opt/rocks/bin/rocks add firewall host=%s rulename=A20-GLOBUS-TCP-PORT-RANGE network=public service="%s" protocol="tcp" action="ACCEPT" chain="INPUT" flags="-m state --state NEW"' % (host,portrange)
+
+				if istest:
+					cmd += ';echo'
+				else:
+					cmd += ';'
+				cmd +=' /opt/rocks/bin/rocks add firewall host=%s rulename=A20-GLOBUS-UDP-PORT-RANGE network=public service="%s" protocol="udp" action="ACCEPT" chain="INPUT" flags="-m state --state NEW"' % (host,portrange)
+
+				if istest:
+					cmd += ';echo'
+				else:
+					cmd += ';'
+				cmd +=' /opt/rocks/bin/rocks add firewall host=%s rulename=A20-GRAM-TCP-PORT network=public service=2119 protocol="tcp" action="ACCEPT" chain="INPUT" flags="-m state --state NEW"' % host
+				if istest:
+					cmd += ';echo'
+				else:
+					cmd += ';'
+				cmd +=' /opt/rocks/bin/rocks sync host firewall %s' % host
+
+
+			if is_osg_ce>0 or istest:
+				p = Parallel(cmd)
+				threads.append(p)
+				p.start()
+
+		#
+		# collect the threads
+		#
+		for thread in threads:
+			thread.join(timeout)
+
+		threads = []
