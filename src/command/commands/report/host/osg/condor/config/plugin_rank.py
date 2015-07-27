4daa78836bccb4f9c0b579f80e637a6258d6f7b5
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2015-07-27 10:03:50 -0400

    add new condor plugin to set rank on nodes. default to zero on executable nodes.

---
+++ src/command/commands/report/host/osg/condor/config/plugin_rank.py
@@ -0,0 +1,26 @@
+import rocks.commands
+
+class Plugin(rocks.commands.Plugin):
+
+	def provides(self):
+		return 'rank'
+
+	def defaultprovisioning(self, host, kvstore):
+                #Default setting
+		kvstore['RANK'] = '0'
+
+	def run(self, argv):
+		# 1. Get the hostname and the key-value store, which
+		#    is a python dictionary 
+		host, kvstore = argv 
+
+		rank = self.db.getHostAttr(host, 'OSG_CONDOR_RANK')
+
+		#default setting (to executable nodes)
+		if "STARTD" in kvstore['DAEMON_LIST']:
+			self.defaultprovisioning(host, kvstore)
+
+		#customized setting (to any daemon)
+		if rank>0:
+			kvstore['RANK'] = rank
+
