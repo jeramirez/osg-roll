234e82d7798caea4646cfb944dd24b32dc5d442f
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2015-10-01 11:20:43 -0400

    added customization on which link is used for linkCVMFS. new attr OSG_wn_CVMFSlink.

--- src/command/commands/report/host/osg/wnclient/config/plugin_Cacert.py
+++ src/command/commands/report/host/osg/wnclient/config/plugin_Cacert.py
@@ -11,13 +11,19 @@ class Plugin(rocks.commands.Plugin):
 		certdir            = configs['Cacert']
 
 		cacert             = self.db.getHostAttr(host,'OSG_wn_Cacert')
+		link               = self.db.getHostAttr(host,'OSG_wn_CVMFSlink')
 
 		addOutput(host, '#begin config %s' % (certdir))
 		addOutput(host, 'rm -f %s' % (certdir))
 		if cacert == 'own':
 			addOutput(host, 'ln -s /etc/grid-security/certificates.osg-ca-certs %s' % (certdir))
 		elif cacert == 'linkCVMFS':
-			addOutput(host, 'ln -s /cvmfs/oasis.opensciencegrid.org/mis/certificates %s' % (certdir))
+			if link > 0:
+				addOutput(host, '#attr OSG_wn_CVMFSlink=%s' % (link))
+				addOutput(host, 'ln -s %s %s' % (link,certdir))
+			else:
+				addOutput(host, '#attr OSG_wn_CVMFSlink not defined, defaulting to "/cvmfs/oasis.opensciencegrid.org/mis/certificates"' )
+				addOutput(host, 'ln -s /cvmfs/oasis.opensciencegrid.org/mis/certificates %s' % (certdir))
 		else:
 #			default to cacert == 'linkCE':
 			addOutput(host, '#attr OSG_wn_Cacert=%s [not "own" nor "linkCVMFS"] defaulting to "linkCE"' % (cacert))
