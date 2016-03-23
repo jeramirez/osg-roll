9d052671613d18afbaf4007841c79f68ffcab0f7
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2016-03-23 11:45:16 -0400

    changed condor default configuration to allow administation/negotiation and prepare interactive nodes conf

--- src/command/commands/report/host/osg/condor/config/plugin_t3cmssw.py
+++ src/command/commands/report/host/osg/condor/config/plugin_t3cmssw.py
@@ -15,9 +15,12 @@ class Plugin(rocks.commands.Plugin):
 		kvstore['STARTIDLETIME']                  = '0'
 		#policy for cmssw jobs
 		#CMSSW jobs cannot be evicted and resumed without loss of compute cycles
-		kvstore['WANT_SUSPEND']                   = 'True'
-		kvstore['SUSPEND']                        = '( (CpuBusyTime &gt; 2 * $(MINUTE)) &amp;&amp; $(ActivationTimer) &gt; 300 )'
-		kvstore['CONTINUE']                       = '$(CPUIdle) &amp;&amp; ($(ActivityTimer) &gt; 10)'
+#		kvstore['WANT_SUSPEND']                   = 'True'
+#		kvstore['SUSPEND']                        = '( (CpuBusyTime &gt; 2 * $(MINUTE)) &amp;&amp; $(ActivationTimer) &gt; 300 )'
+#		kvstore['CONTINUE']                       = '$(CPUIdle) &amp;&amp; ($(ActivityTimer) &gt; 10)'
+		kvstore['WANT_SUSPEND']                   = 'False'
+		kvstore['SUSPEND']                        = 'False'
+		kvstore['CONTINUE']                       = 'True'
 		kvstore['PREEMPT']                        = 'False'
 		#gratia from osg want job reported this directory
 		kvstore['PER_JOB_HISTORY_DIR']            = '/var/lib/gratia/data'
@@ -27,8 +30,16 @@ class Plugin(rocks.commands.Plugin):
 		# test if CMSSW is enabled for the host. If so, set up the
 		# the correct config values in the key,value store
 		t3cmssw_enabled = self.db.getHostAttr(host, 'OSG_Condor_EnableT3GRID_CMSSW')
+		isdesktop       = self.db.getHostAttr(host, 'OSG_Condor_IsDesktop')
 		if t3cmssw_enabled is not None:
 			if (t3cmssw_enabled.lower() == 'true' or t3cmssw_enabled.lower() == 'yes'):
 				self.localt3cmsswdefault(host,kvstore)
-
+				if isdesktop is not None and (isdesktop.lower() == 'true' or isdesktop.lower() == 'yes'):
+					#suspend job after 2 minutes of activity detected
+					kvstore['WANT_SUSPEND']   = 'True'
+					kvstore['SUSPEND']        = '( $(MachineBusy) &amp;&amp; $(ActivityTimer) &gt; (2 * $(MINUTE)) )'
+					#continue job after a minute of Keyboard inactivity and Idle CPU
+					kvstore['CONTINUE']       = '$(CPUIdle) &amp;&amp; KeyboardIdle &gt; (1 * $(MINUTE)) )'
+					#kill job after 20 minutes of activity detected
+					kvstore['KILL']           = '( $(MachineBusy) &amp;&amp; $(ActivityTimer) &gt; (20 * $(MINUTE)) )'
 
