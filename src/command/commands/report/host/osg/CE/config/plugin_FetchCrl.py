331641b80db28d0a1f1b09d430c5fe805bdf0a52
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2017-08-17 11:50:00 -0400

    adding fetcrl plugin to CE configurator

---
+++ src/command/commands/report/host/osg/CE/config/plugin_FetchCrl.py
@@ -0,0 +1,27 @@
+import rocks.commands
+
+class Plugin(rocks.commands.Plugin):
+
+	def provides(self):
+		return 'FetchCrl'
+
+	def run(self, argv):
+		# 1. Get the hostname and the config file to store
+                host, addOutput, configs = argv 
+		Fetchcrl           = configs['FetchCrl']
+
+		squid              = self.db.getHostAttr(host,'OSG_SquidServer')
+		proxy              = self.db.getHostAttr(host,'OSG_CVMFS_HTTP_PROXY')
+
+		addOutput(host, '#begin config %s' % (Fetchcrl))
+		addOutput(host, '/bin/rm -f %s' % (Fetchcrl))
+		addOutput(host, '/bin/touch -f %s' % (Fetchcrl))
+		addOutput(host, 'echo "infodir = &OSG_CE_Export_LocalDir;/ce/globus/share/certificates" &gt;&gt; %s' % (Fetchcrl))
+		if squid>0:
+			addOutput(host, 'echo "http_proxy = http://%s" &gt;&gt; %s' % (squid,Fetchcrl))
+		elif proxy>0:
+			addOutput(host, 'echo "http_proxy = %s" &gt;&gt; %s' % (proxy,Fetchcrl))
+
+		addOutput(host, '#end config %s' % (Fetchcrl))
+		addOutput(host, '')
+
