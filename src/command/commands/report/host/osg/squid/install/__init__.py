a642bf56d3d6856870f3fc13011edd465fc223ad
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2019-07-17 11:14:07 -0400

    preparing frontier squid installer and configurator to work with centos 7

--- src/command/commands/report/host/osg/squid/install/__init__.py
+++ src/command/commands/report/host/osg/squid/install/__init__.py
@@ -36,15 +36,16 @@ class Command(rocks.commands.HostArgumentProcessor,
 		self.beginOutput()
 
                 for host in self.getHostnames(args):
-			self.host = host
+			self.host  = host
 			loginstall = '/var/log/frontier_squid-install.log'
-			osg_squid     = self.db.getHostAttr(host,'OSG_SQUID')
-			if osg_squid > 0 and osg_squid == 'true':
+			osg_squid  = self.db.getHostAttr(host,'OSG_SQUID')
+			is_ce      = self.db.getHostAttr(host,'OSG_CE') > 0
+			if osg_squid or is_ce:
 				self.addOutput(self.host, '/usr/sbin/groupadd -g &OSG_squidgid; squid')
 				self.addOutput(self.host, '/usr/sbin/useradd -r -u &OSG_squiduid; -g &OSG_squidgid; -c "squid management user" -s /sbin/nologin -d /etc/squid squid')
 				self.addOutput(self.host, '')
 				self.addOutput(self.host, 'touch %s' % loginstall)
-				self.addOutput(self.host, 'yum install frontier-squid  &gt;&gt; %s 2&gt;&amp;1' % loginstall)
+				self.addOutput(self.host, 'yum -y install frontier-squid  &gt;&gt; %s 2&gt;&amp;1' % loginstall)
 				self.addOutput(self.host, '')
 
 		self.endOutput(padChar='')
