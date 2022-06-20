6b9f3bd1827c8f70cb2d73742b4763a39159e7fa
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2022-06-20 16:41:28 -0400

    code ready to make rocks attrs OSG_condorgid, OSG_condoruid optional.

--- src/command/commands/report/host/osg/condor/install/__init__.py
+++ src/command/commands/report/host/osg/condor/install/__init__.py
@@ -39,20 +39,46 @@ class Command(rocks.commands.HostArgumentProcessor,
 			self.host = host
 			osg_condor  = self.db.getHostAttr(host,'OSG_Condor_Client')
 			osg_ce      = self.db.getHostAttr(host,'OSG_CE')
+			condorgid   = self.db.getHostAttr(host,'OSG_condorgid')
+			condoruid   = self.db.getHostAttr(host,'OSG_condoruid')
 			config02    = '/etc/condor/config.d/02_rocks_condor_config.local'
 
 			if (osg_condor > 0 and osg_condor == 'true') or (osg_ce > 0 and osg_ce == 'condor'):
-				self.addOutput(self.host, '/usr/sbin/groupadd -g &OSG_condorgid; condor')
-				self.addOutput(self.host, '/usr/sbin/useradd -r -u &OSG_condoruid; -g &OSG_condorgid; -c "Owner of Condor Daemons" -s /bin/nologin -d /var/lib/condor condor')
+#				self.addOutput(self.host, '/usr/sbin/groupadd -g &OSG_condorgid; condor')
+#				self.addOutput(self.host, '/usr/sbin/useradd -r -u &OSG_condoruid; -g &OSG_condorgid; -c "Owner of Condor Daemons" -s /bin/nologin -d /var/lib/condor condor')
 				self.addOutput(self.host, 'touch /var/log/condor-install.log')
 				self.addOutput(self.host, 'yum -y install condor  &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+				if (condorgid > 0):
+					self.addOutput(self.host, 'swapgid=`/usr/bin/getent group condor | cut -d: -f3`')
+					self.addOutput(self.host, 'echo swapgid=$swapgid &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+					self.addOutput(self.host, 'swapgroup=`/usr/bin/getent group &OSG_condorgid; | cut -d: -f1`')
+					self.addOutput(self.host, 'echo swapgroup=$swapgroup &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+					self.addOutput(self.host, '[ ! -z "$swapgroup" ]&amp;&amp;[ "x$swapgroup" != "xcondor" ]&amp;&amp;/usr/sbin/groupmod -o -g $swapgid $swapgroup')
+					self.addOutput(self.host, '[ ! -z "$swapgroup" ]&amp;&amp;[ "x$swapgroup" != "xcondor" ]&amp;&amp;echo "/usr/sbin/groupmod -o -g $swapgid $swapgroup" &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+					self.addOutput(self.host, '[ ! -z "$swapgroup" ]&amp;&amp;[ "x$swapgroup" != "xcondor" ]&amp;&amp;/usr/sbin/groupmod -g &OSG_condorgid; condor')
+					self.addOutput(self.host, '[ ! -z "$swapgroup" ]&amp;&amp;[ "x$swapgroup" != "xcondor" ]&amp;&amp;echo "/usr/sbin/groupmod -g &OSG_condorgid; condor" &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+				if (condoruid > 0):
+					self.addOutput(self.host, 'swapuid=`/usr/bin/getent passwd condor | cut -d: -f3`')
+					self.addOutput(self.host, 'echo swapuid=$swapuid &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+					self.addOutput(self.host, 'swapuser=`/usr/bin/getent passwd &OSG_condoruid; | cut -d: -f1`')
+					self.addOutput(self.host, 'echo swapuser=$swapuser &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+					self.addOutput(self.host, '[ ! -z "$swapuser" ]&amp;&amp;[ "x$swapuser" != "xcondor" ]&amp;&amp;/usr/sbin/usermod -o -u $swapuid $swapuser')
+					self.addOutput(self.host, '[ ! -z "$swapuser" ]&amp;&amp;[ "x$swapuser" != "xcondor" ]&amp;&amp;echo "/usr/sbin/usermod -o -u $swapuid $swapuser" &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
+					self.addOutput(self.host, '[ ! -z "$swapuser" ]&amp;&amp;[ "x$swapuser" != "xcondor" ]&amp;&amp;/usr/sbin/usermod -u &OSG_condoruid; -g &OSG_condorgid;  condor')
+					self.addOutput(self.host, '[ ! -z "$swapuser" ]&amp;&amp;[ "x$swapuser" != "xcondor" ]&amp;&amp;echo "/usr/sbin/usermod -u &OSG_condoruid; -g &OSG_condorgid;  condor" &gt;&gt; /var/log/condor-install.log 2&gt;&amp;1')
 				self.addOutput(self.host, '')
-				self.addOutput(self.host, '#Make sure ownership is fine')
-				self.addOutput(self.host, '/bin/chown -R condor:condor /var/log/condor')
-				self.addOutput(self.host, '/bin/chown -R condor:condor /var/run/condor')
-				self.addOutput(self.host, '/bin/chown -R condor:condor /var/lib/condor')
+				if (condoruid > 0 or condorgid > 0):
+					self.addOutput(self.host, '#Make sure ownership is fine')
+					self.addOutput(self.host, '/bin/chown condor:condor /var/log/condor')
+					self.addOutput(self.host, '/bin/chown condor:condor /var/lib/condor')
+					self.addOutput(self.host, '/bin/chown condor:condor /var/lib/condor/spool')
+					self.addOutput(self.host, '/bin/chown condor:condor /var/lib/condor/execute')
+					self.addOutput(self.host, '/bin/chgrp condor /var/lib/condor/oauth_credentials')
 				self.addOutput(self.host, '/bin/mkdir -p /var/lib/condor/cred_dir')
 				self.addOutput(self.host, '')
+#				With large memory footprints, java doesn't start. See
+#				http://www.cs.wisc.edu/condor/manual/v6.8/3_14Java_Support.html#sec:java-install
+#				for workaround used here (Most likely obsolete JAVA_MAXHEAP_ARGUMENT in condor v9_0)
 				self.addOutput(self.host, '#Write Java part:%s (if needed)' % (config02) )
 				self.addOutput(self.host, '[ -f %s ] || touch %s' % (config02,config02) )
 				self.addOutput(self.host, '[ "`grep -c First %s`" != "0"  ] ||echo "# First set JAVA_MAXHEAP_ARGUMENT to null, to disable the default of max RAM" &gt;&gt; %s' % (config02,config02) )
